5173df33d26c70d89f2cabd2f0f3098c3bdc5ba1
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-03-07 12:43:23 +0500

    fixed

--- app/Filament/Resources/PhoneNumberResource/Pages/ListPhoneNumbers.php
+++ app/Filament/Resources/PhoneNumberResource/Pages/ListPhoneNumbers.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\PhoneNumberResource\Pages;
 
 use App\Filament\Resources\PhoneNumberResource;
+use App\Imports\PhoneNumberImport;
 use Filament\Actions;
+use Filament\Forms;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Notifications\Notification;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListPhoneNumbers extends ListRecords
 {
@@ -13,6 +17,30 @@ class ListPhoneNumbers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('import')
+                ->label('Exceldan import')
+                ->icon('heroicon-o-arrow-up-tray')
+                ->form([
+                    Forms\Components\FileUpload::make('file')
+                        ->label('Excel fayl')
+                        ->acceptedFileTypes([
+                            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            'application/vnd.ms-excel',
+                            'text/csv',
+                        ])
+                        ->required(),
+                ])
+                ->action(function (array $data): void {
+                    $import = new PhoneNumberImport();
+
+                    Excel::import($import, storage_path('app/public/' . $data['file']));
+
+                    Notification::make()
+                        ->title('Import yakunlandi')
+                        ->body("Qo'shildi: {$import->imported} ta, Takrorlangan: {$import->duplicated} ta")
+                        ->success()
+                        ->send();
+                }),
             Actions\CreateAction::make(),
         ];
     }
---
+++ app/Imports/PhoneNumberImport.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\PhoneNumber;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class PhoneNumberImport implements ToModel, WithHeadingRow
+{
+    public int $imported = 0;
+    public int $duplicated = 0;
+
+    public function model(array $row): ?PhoneNumber
+    {
+        $number = trim($row['number'] ?? $row[0] ?? '');
+
+        if (empty($number)) {
+            return null;
+        }
+
+        if (PhoneNumber::where('number', $number)->exists()) {
+            $this->duplicated++;
+            return null;
+        }
+
+        $this->imported++;
+
+        return new PhoneNumber([
+            'number' => $number,
+            'is_active' => true,
+        ]);
+    }
+}
